bd0d46a69e8b0e7f2fcfa11b04b8b2be724abc29
UmidaJamshidbekovna <78403650+UmidaJamshidbekovna@users.noreply.github.com>
2025-06-30 13:10:35 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import Navbar from '@/views/Navbar/Navbar.vue'
+</script>
+
+<template>
+  
+  <Navbar/>
+  
+</template>
+
---
+++ src/views/Navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+import folioLogo from "@/assets/svg/folio-logo.svg"
+</script>
+
+<template>
+  <div class="container flex ">
+
+    Logo
+
+    <img :src="folioLogo" alt="Folio Logo" />
+  </div>
+</template>
